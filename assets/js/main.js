ab97afb9830330ba0e6f1cf187873ffc8499f203
skabuamer <shaikhabuamer@gmail.com>
2022-07-02 14:39:31 +0600

    portfolio and price section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,26 @@ document.getElementById('demo').addEventListener("click", vid => {
 document.querySelector('#popup-video span').onclick = () => {
     document.getElementById("popup-video").style.display = 'none';
 }
+
+// document.querySelectorAll("#portfolio-section .row img").forEach(imag => {
+//     imag.onclick = () => {
+//         document.querySelector('.popup-image').style.display = 'block';
+//         document.querySelector('.popup-image img').src = imag.getAttribute('src');
+//     }
+// })
+
+// document.querySelector('.single-portfolio-info svg').onclick = () => {
+//     document.getElementById("popup-image").style.display = 'block';
+// }
+
+// document.querySelectorAll('.single-portfolio img').forEach(imag => {
+//     console.log(imag.outerHTML)
+//     // document.getElementById("popup-image").style.display = 'block';
+
+//     // document.querySelectorAll("#portfolio-section .row img").forEach(imag => {
+//         imag.onclick = () => {
+//             document.querySelector('.popup-image').style.display = 'block';
+//             document.querySelector('.popup-image img').src = imag.getAttribute('src');
+//         }
+//     // })
+// })
